a4f7f38042ac4a3f4737f9628f2fb26f232f9a4b
connor <vbrookesy@hotmail.co.uk>
2020-07-01 14:15:43 +0100

    Initial check-in of module TT_Project

---
+++ TT_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TT_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TT_Project/Models/MovieModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TT_Project.Models
+{
+    public class MovieModel
+    {
+        
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Rating { get; set; }
+        public string ReleaseDate { get; set; }
+        public int IMDbscore { get; set; }
+
+    }
+}
+
